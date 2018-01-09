6c369ed93bad60c6cdd6909d2e2fa561c66ae241
Nouri <s.nouri2005@gmail.com>
2018-01-09 17:19:00 +0330

    dropdown inherit

--- Tadbir.GridViewCRUD/ClientApp/app/controls/sppc-dropdownlist.ts
+++ Tadbir.GridViewCRUD/ClientApp/app/controls/sppc-dropdownlist.ts
@@ -1,35 +1,44 @@
 ﻿
-import { Component, OnInit, Input } from '@angular/core';
-import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { DropDownsModule } from "@progress/kendo-angular-dropdowns";
+
 
 @Component({
     selector: 'sppc-dropdownlist',
     template: `<kendo-dropdownlist [data]="data" [valuePrimitive]="true"
-                                    [textField]="'Value'" formControlName="fiscalPeriodId" [value]="selectedValue" [(ngModel)]="selectedValue" [valueField]="'Key'">
-                </kendo-dropdownlist>`,
-    providers: [{
-        provide: NG_VALUE_ACCESSOR,
-        useExisting: SppcDropDownList,
-        multi: true
-    }]
+                                    [textField]="textField"  [value]="selectedValue" [valueField]="valueField" [(ngModel)]="selectedValue" ></kendo-dropdownlist>` ,
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: forwardRef(() => SppcDropDownList),
+        }
+    ]
 })
 
-export class SppcDropDownList implements OnInit, ControlValueAccessor {
-
+export class SppcDropDownList extends DropDownsModule implements OnInit,ControlValueAccessor {
     writeValue(obj: any): void {
-        throw new Error("Method not implemented.");
+        this.selectedValue = obj;      
     }
-    registerOnChange(fn: any): void {
-        throw new Error("Method not implemented.");
+    registerOnChange(fn: any): void {        
     }
-    registerOnTouched(fn: any): void {
-        throw new Error("Method not implemented.");
+    registerOnTouched(fn: any): void {        
     }
-    
+
+    public selectedValue: string;
+    @Input() public data: any;
+    @Input() public textField: string; 
+    @Input() public valueField: string; 
+    //public fiscalPeriodId: string; 
+
     ngOnInit() {
         
     }
-    
+
+    constructor()
+    {
+        super();
+    }
 
 }
