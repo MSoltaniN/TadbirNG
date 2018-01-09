d3745fea83196891902d770cd3c966f7bc71d74d
SNouri2005 <S.Nouri2005@yahoo.com>
2018-01-10 00:36:08 +0330

    fix some bug in SppcDropDownList

--- Tadbir.GridViewCRUD/ClientApp/app/controls/sppc-dropdownlist.ts
+++ Tadbir.GridViewCRUD/ClientApp/app/controls/sppc-dropdownlist.ts
@@ -1,13 +1,18 @@
 ﻿
-import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { Component, OnInit, Input, forwardRef, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { DropDownsModule } from "@progress/kendo-angular-dropdowns";
+
+const noop = () => {
+};
+
 
 
 @Component({
     selector: 'sppc-dropdownlist',
     template: `<kendo-dropdownlist [data]="data" [valuePrimitive]="true"
-                                    [textField]="textField"  [value]="selectedValue" [valueField]="valueField" [(ngModel)]="selectedValue" ></kendo-dropdownlist>` ,
+                                    [textField]="textField" (valueChange)="valueChange($event)"  [value]="selectedValue" [valueField]="valueField"
+                [(ngModel)]="currentValue" ></kendo-dropdownlist>` ,
     providers: [
         {
             provide: NG_VALUE_ACCESSOR,
@@ -17,15 +22,32 @@ import { DropDownsModule } from "@progress/kendo-angular-dropdowns";
     ]
 })
 
-export class SppcDropDownList extends DropDownsModule implements OnInit,ControlValueAccessor {
+export class SppcDropDownList implements OnInit, ControlValueAccessor {
+
+    private onTouchedCallback: () => void = noop;
+    private onChangeCallback: (_: any) => void = noop;
+
+
     writeValue(obj: any): void {
+        
         this.selectedValue = obj;      
     }
-    registerOnChange(fn: any): void {        
-    }
-    registerOnTouched(fn: any): void {        
+
+    //From ControlValueAccessor interface
+    registerOnChange(fn: any) {
+        this.onChangeCallback = fn;
     }
 
+    //From ControlValueAccessor interface
+    registerOnTouched(fn: any) {
+        this.onTouchedCallback = fn;
+    }
+
+    public valueChange(value: any): void {
+        this.selectedValue = value;
+    }
+    
+    
     public selectedValue: string;
     @Input() public data: any;
     @Input() public textField: string; 
@@ -38,7 +60,26 @@ export class SppcDropDownList extends DropDownsModule implements OnInit,ControlV
 
     constructor()
     {
-        super();
+        
+    }
+
+
+    //get accessor
+    get currentValue(): any {
+        return this.selectedValue;
+    };
+
+    //set accessor including call the onchange callback
+    set currentValue(v: any) {
+        if (v !== this.selectedValue) {
+            this.selectedValue = v;
+            this.onChangeCallback(v);
+        }
+    }
+
+    //Set touched on blur
+    onBlur() {
+        this.onTouchedCallback();
     }
 
 }
