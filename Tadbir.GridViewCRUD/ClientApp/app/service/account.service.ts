134fe4e6d55839d7978621bd2fe8cc183dfd1881
SNouri2005 <S.Nouri2005@yahoo.com>
2018-01-17 01:57:33 +0330

    change services for integrate with .net core

--- Tadbir.GridViewCRUD/ClientApp/app/service/account.service.ts
+++ Tadbir.GridViewCRUD/ClientApp/app/service/account.service.ts
@@ -15,24 +15,28 @@ export class AccountInfo implements Account
     
 }
 
+const BASE_URL = "http://130.185.76.7:8080";
+const ADMIN_TICKET = "eyJVc2VyIjp7IklkIjoxLCJQZXJzb25GaXJzdE5hbWUiOiIiLCJQZXJzb25MYXN0TmFtZSI6IiIsIkJyYW5jaGVzIjpbMSwyXSwiUm9sZXMiOlsxXSwiUGVybWlzc2lvbnMiOlt7IkVudGl0eU5hbWUiOiJBY2NvdW50IiwiRmxhZ3MiOjE1fSx7IkVudGl0eU5hbWUiOiJUcmFuc2FjdGlvbiIsIkZsYWdzIjoxMDIzfSx7IkVudGl0eU5hbWUiOiJVc2VyIiwiRmxhZ3MiOjd9LHsiRW50aXR5TmFtZSI6IlJvbGUiLCJGbGFncyI6NjN9LHsiRW50aXR5TmFtZSI6IlJlcXVpc2l0aW9uVm91Y2hlciIsIkZsYWdzIjoxMjd9LHsiRW50aXR5TmFtZSI6Iklzc3VlUmVjZWlwdFZvdWNoZXIiLCJGbGFncyI6NjN9LHsiRW50aXR5TmFtZSI6IlNhbGVzSW52b2ljZSIsIkZsYWdzIjozMX0seyJFbnRpdHlOYW1lIjoiUHJvZHVjdEludmVudG9yeSIsIkZsYWdzIjoxNX1dfX0=";
+const FP_ID = 1
+const BRANCH_ID = 1
+
+
 @Injectable()
 export class AccountService 
-{
-    private _getAccountsUrl = "/Account/fp/{0}/branch/{1}";
+{   
+    private _getAccountsUrl = BASE_URL + "/accounts/fp/{0}/branch/{1}";
 
-    private _getAllAccountsUrl = "/Account/List";
+    private _getAllAccountsUrl = BASE_URL + "/accounts";
 
-    private _getTotalCountUrl = "/Account/TotalCount";
+    private _getTotalCountUrl = BASE_URL + "/Account/TotalCount";
 
-    private _getCountUrl = "/Account/Count";
+    private _getCountUrl = BASE_URL + "/accounts/fp/{0}/branch/{1}/count";
 
-    private _deleteUrl = "/Account/Delete/{0}";
+    private _deleteAccountsUrl = BASE_URL + "/Account/DeleteAccs";
 
-    private _deleteAccountsUrl = "/Account/DeleteAccs";
+    private _postNewAccountsUrl = BASE_URL + "/accounts";
 
-    private _postNewAccountsUrl = "/Account/Insert";
-
-    private _postModifiedAccountsUrl = "/Account/Edit";
+    private _postModifiedAccountsUrl = BASE_URL + "/accounts";
 
     headers: Headers;
     options: RequestOptions;
@@ -40,15 +44,14 @@ export class AccountService
     constructor(private http: Http)
     {
 
-        this.headers = new Headers({
-            'Content-Type': 'application/json'            
-        });
-        this.options = new RequestOptions({ headers: this.headers });
-
+        this.headers = new Headers({ 'Content-Type': 'application/json; charset=utf-8'});        
+        this.headers.append('X-Tadbir-AuthTicket', ADMIN_TICKET);
+        
+        this.options = new RequestOptions({ headers: this.headers });        
     }
 
     getAccounts() {
-        var headers = new Headers();
+        var headers = this.headers;
         headers.append("If-Modified-Since", "Tue, 24 July 2017 00:00:00 GMT");
         var url = this._getAccountsUrl;
         return this.http.get(url, { headers: headers })
@@ -56,98 +59,63 @@ export class AccountService
     }
 
     getTotalCount() {
-        var headers = new Headers();
+        var headers = this.headers;
         headers.append("If-Modified-Since", "Tue, 24 July 2017 00:00:00 GMT");
-        var url = this._getTotalCountUrl;
+
+        var url = String.Format(this._getCountUrl, FP_ID, BRANCH_ID);
+        
         return this.http.get(url, { headers: headers })
             .map(response => <any>(<Response>response).json());;
-        
             
     }
 
     //get count of records base on Grid filters and order value
     getCount(orderby?: string, filters?: any[]) {
-        var headers = new Headers();
-        headers.append("If-Modified-Since", "Tue, 24 July 2017 00:00:00 GMT");
-        var url = this._getCountUrl;
+        var headers = this.headers;
+        //headers.append("If-Modified-Since", "Tue, 24 July 2017 00:00:00 GMT");
 
-        var postItem = { Filters: filters, OrderBy: orderby };
+        var url = String.Format(this._getCountUrl, FP_ID, BRANCH_ID);
 
-        return this.http.post(url,postItem, { headers: headers })
-            .map(response => <any>(<Response>response).json());;
+        //var postItem = { Filters: filters, OrderBy: orderby };
+        
+        //return this.http.post(url,postItem, { headers: headers })
+        //    .map(response => <any>(<Response>response).json());;
+        let options = new RequestOptions({ headers: this.headers });
 
+            return this.http.get(url, options)
+                .map(response => <any>(<Response>response).json());;
 
     }
 
     
     
     search(start? :number , count? :number , orderby?:string,filters?:Filter[]  ) {
-        var headers = new Headers({ 'Content-Type': 'application/json' });
+        var headers = this.headers;
         var options = new RequestOptions({ headers: headers });
-
-        //this path for filter branch and fiscalpriod
-        //var url = this._getAccountsUrl;  
-
-        var url = this._getAllAccountsUrl;
-
-
-
-
-
-        /*
-        let params: URLSearchParams = new URLSearchParams();
-
-              
-        if(start != undefined && count != undefined)
-        {
-            params.append("start", start.toString());
-            params.append("count", count.toString());    
-        }        
-
         
-        if(filters)
-        {
-            params.set("filter", JSON.stringify(filters));     
-        }
-
-
-
-        if(orderby)
-        {
-            params.set("filter", orderby);     
-        }*/
-
         var postItem = { StartIndex: start, Count: count, Filters: filters, OrderBy: orderby };
         
-
-        /*
-        var fpId = '1';
-        var branchId = '1';
-
-        var newUrl = String.Format(this._getAccountsUrl, fpId, branchId);
-        */
-
-        var newUrl = this._getAllAccountsUrl;
-
-        return this.http.post(newUrl, JSON.stringify(postItem), options)
+        var url = String.Format(this._getAccountsUrl, FP_ID, BRANCH_ID);
+        
+        return this.http.post(url, JSON.stringify(postItem), options)
             .map(response => <any>(<Response>response).json());
     }
 
     
     editAccount(account: Account): Observable<string> {
-        let body = JSON.stringify(account);
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers });
+        var body = JSON.stringify(account);
+        var headers = this.headers;
+        var options = new RequestOptions({ headers: headers });
 
-        return this.http.post(this._postModifiedAccountsUrl, body, options)
+        return this.http.put(this._postModifiedAccountsUrl, body, options)
             .map(res => res.json().message)
             .catch(this.handleError);
     }
 
     insertAccount(account: Account): Observable<string> {
-        let body = JSON.stringify(account);
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers });
+        var body = JSON.stringify(account);
+        var headers = this.headers;
+        var options = new RequestOptions({ headers: headers });
 
         return this.http.post(this._postNewAccountsUrl, body, options)
             .map(res => res.json().message)
@@ -158,18 +126,20 @@ export class AccountService
     {
         //ToDo : call api for delete entity
 
-        var deleteByIdUrl = String.Format(this._deleteUrl, accountId.toString());
+        var deleteByIdUrl = String.Format(this._deleteAccountsUrl, accountId.toString());
 
-        return this.http.post(deleteByIdUrl,this.options)
+        return this.http.delete(deleteByIdUrl,this.options)
             .map(response => response.json().message)
             .catch(this.handleError);
     }
 
+    // this method comment beacause method in controller not implemented
+    
     deleteAccounts(accounts: string[]): Observable<string> {
         //ToDo : call api for delete entity
 
         let body = JSON.stringify(accounts);
-        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let headers = this.headers
         let options = new RequestOptions({ headers: headers });
 
 
@@ -177,6 +147,7 @@ export class AccountService
             .map(response => response.json().message)
             .catch(this.handleError);
     }
+    
 
     private handleError(error: Response) {
         return Observable.throw(error.json().error || 'Opps!! Server error');
