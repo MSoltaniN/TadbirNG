402bfc27ed79addfb9b84e67b0d3488504efb33a
SNouri2005 <S.Nouri2005@yahoo.com>
2018-01-31 01:07:11 +0330

    change caption

--- SPPC.Tadbir.Web/ClientApp/app/service/message/sppc-message.service.ts
+++ SPPC.Tadbir.Web/ClientApp/app/service/message/sppc-message.service.ts
@@ -5,7 +5,7 @@ import { TranslateService } from "ng2-translate";
 
 var messages : { [id: string]: string; } = {
     "filterEqOperator": "مساوی",
-    "filterNotEqOperator" : "نا مساوی",
+    "filterNotEqOperator" : "نامساوی",
     "filterIsNullOperator": "مساوی با تهی",
     "filterIsNotNullOperator": "مخالف با تهی",
     "filterIsEmptyOperator": "مساوی با خالی",
