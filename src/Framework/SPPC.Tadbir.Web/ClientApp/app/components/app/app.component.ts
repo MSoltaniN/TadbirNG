018df6592dc815609dee1c9842c76b62513452de
Administrator <Administrator@ASUS-PC>
2018-04-11 01:39:53 +0430

    add enviroment variables to dashboard

--- src/Framework/SPPC.Tadbir.Web/ClientApp/app/components/app/app.component.ts
+++ src/Framework/SPPC.Tadbir.Web/ClientApp/app/components/app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Context } from "../../model/context";
 import { Router, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { FiscalPeriodService } from '../../service/fiscal-period.service';
+import { CompanyService } from '../../service/company.service';
+import { BranchService } from '../../service/branch.service';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
     selector: 'app',
@@ -16,23 +20,26 @@ export class AppComponent {
 
     public isLogin: boolean = false;
 
+    public isRtl: boolean;
+
     public lang: string = '';
 
-    //template: string = `<div class="sk-cube-grid">
-    //    <div class="sk-cube sk-cube1"></div>
-    //    <div class="sk-cube sk-cube2"></div>
-    //    <div class="sk-cube sk-cube3"></div>
-    //    <div class="sk-cube sk-cube4"></div>
-    //    <div class="sk-cube sk-cube5"></div>
-    //    <div class="sk-cube sk-cube6"></div>
-    //    <div class="sk-cube sk-cube7"></div>
-    //    <div class="sk-cube sk-cube8"></div>
-    //    <div class="sk-cube sk-cube9"></div>
-    //</div>`;
+    public companyName: string;
+    public branchName: string;
+    public fiscalPeriodName: string;
+    public userName: string;
 
-    //public langIsFa: boolean = false;
-    
-    constructor(location: Location,router: Router) {
+    public compenies: any = {};
+
+    public branches: any = {};
+
+    public fiscalPeriods: any = {};
+
+
+
+    constructor(location: Location, router: Router, public branchService: BranchService,
+        public companyService: CompanyService,
+        private fiscalPeriodService: FiscalPeriodService, @Inject(DOCUMENT) private document: Document) {
 
 
         if (localStorage.getItem('currentContext') != null) {
@@ -47,12 +54,9 @@ export class AppComponent {
         }
         else {
             this.lang = "fa";
-        }
+        }        
 
-        //if (this.lang == "fa")
-        //    this.langIsFa = true;
-        //else
-        //    this.langIsFa = false;
+        
 
         if (this.currentContext != undefined) {
             this.showNavbar = true;
@@ -61,6 +65,7 @@ export class AppComponent {
         router.events.subscribe((val) => {
             if (location.path().toLowerCase() == '/login' || location.path().toString().indexOf('/login?returnUrl=') >= 0) {
                 this.showNavbar = false;
+
                 this.isLogin = true;
                 
             }
@@ -68,11 +73,87 @@ export class AppComponent {
             {
                 this.isLogin = false;
                 this.showNavbar = true;
+
+                var spacePad = this.document.getElementById('spacePad')
+                var currentLang = localStorage.getItem('lang')
+                if (currentLang == 'fa') {
+                    if (spacePad) {
+                        spacePad.classList.add('pull-right');
+                        spacePad.classList.remove('pull-left');
+                    }
+                }
+                else {
+                    if (spacePad) {
+                        spacePad.classList.add('pull-left');
+                        spacePad.classList.remove('pull-right');
+                    }
+                }
+
+
+        var branchId: number = 0;
+        var companyId: number = 0;
+        var fpId: number = 0;
+        var ticket: string = "";        
+        
+
+        var contextIsEmpty: boolean = true;
+
+        if (localStorage.getItem('currentContext') != null) {
+            var item: string | null;
+            item = localStorage.getItem('currentContext');
+            var currentContext = JSON.parse(item != null ? item.toString() : "");
+
+            branchId = currentContext ? parseInt(currentContext.branchId) : 0;
+            companyId = currentContext ? parseInt(currentContext.companyId) : 0;
+            fpId = currentContext ? parseInt(currentContext.fpId) : 0;
+            ticket = currentContext ? currentContext.ticket : "";
+            this.userName = currentContext ? currentContext.userName.toString() : "";
+            
+
+            contextIsEmpty = false;
+        }
+        else if (sessionStorage.getItem('currentContext') != null) {
+            var item: string | null;
+            item = sessionStorage.getItem('currentContext');
+            var currentContext = JSON.parse(item != null ? item.toString() : "");
+
+            branchId = currentContext ? parseInt(currentContext.branchId) : 0;
+            companyId = currentContext ? parseInt(currentContext.companyId) : 0;
+            fpId = currentContext ? parseInt(currentContext.fpId) : 0;
+            ticket = currentContext ? currentContext.ticket.toString() : "";
+            this.userName = currentContext ? currentContext.userName.toString() : "";
+            
+
+            contextIsEmpty = false;
+        }
+
+        if (!contextIsEmpty) {
+
+           
+
+            this.fiscalPeriodService.getFiscalPeriod(companyId).subscribe(res => {
+                //this.fiscalPeriods = res;
+                this.fiscalPeriodName = res.filter((p: any) => p.key == fpId)[0].value;
+            });
+
+            this.branchService.getBranches(companyId).subscribe(res => {
+                this.branchName = res.filter((p: any) => p.key == branchId)[0].value;
+            });
+
+
+            var companiesList = this.companyService.getCompanies(this.userName, ticket);
+            if (companiesList != null) {
+                companiesList.subscribe(res => {
+                    this.companyName = res.filter((p: any) => p.key == companyId)[0].value;;
+
+                });
+            }
+
+        }
+
             }
         });
-        
 
         
-
     }
 }
