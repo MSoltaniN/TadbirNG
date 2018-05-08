25e225618fcd78d0fd958a9c92d7ca275ee0652f
b-eslamieh <b_eslamieh@sppc.co.ir>
2018-05-08 19:30:26 +0430

    Added "tsapi" command to ProjectCLI (Build 281)

--- src/Framework/SPPC.Framework.Tools.ProjectCLI/Program.cs
+++ src/Framework/SPPC.Framework.Tools.ProjectCLI/Program.cs
@@ -10,19 +10,43 @@ namespace SPPC.Framework.Tools.ProjectCLI
     {
         static int Main(string[] args)
         {
+            DisplayBanner();
             if (args.Length == 0)
             {
                 Console.WriteLine("No parameter(s) specified.");
+                Console.ReadLine();
                 return -1;
             }
 
-            string[] typesParam = GetParameter(args, _typesParam)
-                .Split(',');
-            foreach (string param in typesParam)
+            string[] supportedCommands = new string[] { "tsvm", "tsapi" };
+            string command = GetParameter(args, _commandParam);
+            if (String.IsNullOrEmpty(command))
             {
-                GenerateTypescriptModel(param);
+                Console.ReadLine();
+                return -1;
             }
 
+            if (!supportedCommands.Contains(command))
+            {
+                Console.WriteLine("Command '{0}' is not supported.", command);
+                Console.ReadLine();
+                return -2;
+            }
+
+            string types = GetParameter(args, _typesParam);
+            if (String.IsNullOrEmpty(types))
+            {
+                Console.ReadLine();
+                return -1;
+            }
+
+            foreach (string param in types.Split(','))
+            {
+                HandleGenerationCommand(command, param);
+            }
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey(true);
             return 0;
         }
 
@@ -38,6 +62,18 @@ namespace SPPC.Framework.Tools.ProjectCLI
             }
 
             return param;
+        }
+
+        private static void HandleGenerationCommand(string command, string typeName)
+        {
+            if (command.ToLower() == "tsvm")
+            {
+                GenerateTypescriptModel(typeName);
+            }
+            else if (command.ToLower() == "tsapi")
+            {
+                GenerateTypescriptApi(typeName);
+            }
         }
 
         private static void GenerateTypescriptModel(string typeName)
@@ -62,6 +98,36 @@ namespace SPPC.Framework.Tools.ProjectCLI
             }
         }
 
+        private static void GenerateTypescriptApi(string typeName)
+        {
+            string tsApiPath = ConfigurationManager.AppSettings["TsApiPath"];
+            string csAssembly = ConfigurationManager.AppSettings["CsApiAssembly"];
+            string csNamespace = ConfigurationManager.AppSettings["CsApiNamespace"];
+            string csOutPath = ConfigurationManager.AppSettings["CsOutputPath"];
+            string tsTypeName = typeName.CamelCase();
+            string fullName = String.Format("{0}.{1}", csNamespace, typeName);
+            string generatedPath = String.Format(@"{0}\{1}.ts", tsApiPath, tsTypeName);
+            string assemblyPath = String.Format(@"{0}\{1}.dll", csOutPath, csAssembly);
+            var assembly = Assembly.Load(csAssembly);
+            if (assembly != null)
+            {
+                var csType = assembly.GetType(fullName);
+                var template = new TsApiFromCsApi(csType);
+                File.WriteAllText(generatedPath, template.TransformText());
+            }
+        }
+
+        private static void DisplayBanner()
+        {
+            Console.WriteLine();
+            Console.WriteLine("============================================================");
+            Console.WriteLine("SPPC Framework : Project Command-Line Interface (v1.1)");
+            Console.WriteLine("(c) Copyright 2018, SPPC, All Rights Reserved");
+            Console.WriteLine("============================================================");
+            Console.WriteLine();
+        }
+
+        private static readonly string _commandParam = "-cmd:";
         private static readonly string _typesParam = "-types:";
     }
 }
