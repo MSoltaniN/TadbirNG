3f611fff4644201606a0267a536b7d3036d08ad5
saeed ghasemi <s.ghasemi365@gmail.com>
2018-05-14 17:37:53 +0430

    Refactor login validation (UI - Build 208)

--- src/Framework/SPPC.Tadbir.Web/ClientApp/app/components/login/login.complete.component.ts
+++ src/Framework/SPPC.Tadbir.Web/ClientApp/app/components/login/login.complete.component.ts
@@ -151,19 +151,19 @@ export class LoginCompleteComponent extends DefaultComponent implements OnInit {
         var isValidate: boolean = true;
 
         if (this.companyId == '') {
-            this.showMessage(this.getText("Login.Validation.SelectCompany"), MessageType.Info, '', MessagePosition.TopCenter);
+            this.showMessage(this.getText("AllValidations.Login.CompanyIsRequired"), MessageType.Info, '', MessagePosition.TopCenter);
             isValidate = false;
             return isValidate;
         }
 
         if (this.branchId == '')
         {
-            this.showMessage(this.getText("Login.Validation.SelectBranch"), MessageType.Info, '', MessagePosition.TopCenter);
+            this.showMessage(this.getText("AllValidations.Login.BranchIsRequired"), MessageType.Info, '', MessagePosition.TopCenter);
             isValidate = false;
         }
 
         if (this.fiscalPeriodId == '') {
-            this.showMessage(this.getText("Login.Validation.SelectFiscalPeriod"), MessageType.Info, '', MessagePosition.TopCenter);
+            this.showMessage(this.getText("AllValidations.Login.FiscalPeriodIsRequired"), MessageType.Info, '', MessagePosition.TopCenter);
             isValidate = false;
         }
 
