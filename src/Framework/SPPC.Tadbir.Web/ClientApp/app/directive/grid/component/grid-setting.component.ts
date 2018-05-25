a6dc724471e2c615879fdca2f6ee9649e7132476
Administrator <Administrator@ASUS-PC>
2018-05-26 02:17:06 +0430

    fix some bugs in GridSetting form

--- src/Framework/SPPC.Tadbir.Web/ClientApp/app/directive/grid/component/grid-setting.component.ts
+++ src/Framework/SPPC.Tadbir.Web/ClientApp/app/directive/grid/component/grid-setting.component.ts
@@ -1,6 +1,6 @@
 ﻿import { RTL } from "@progress/kendo-angular-l10n";
 import { Layout } from "../../../enviroment";
-import { Component, OnInit, ViewContainerRef, Host } from "@angular/core";
+import { Component, OnInit, ViewContainerRef, Host, ElementRef } from "@angular/core";
 import { ToastrService } from "ngx-toastr";
 import { TranslateService } from "ng2-translate";
 import { SppcLoadingService } from "../../../controls/sppcLoading/index";
@@ -32,7 +32,8 @@ export class GridSettingComponent extends BaseComponent implements OnInit {
     public rowData: any[] = [];
     //public rowData: GridDataResult;
 
-    constructor(public toastrService: ToastrService, public translate: TranslateService, @Host() private grid: GridComponent) {
+    constructor(public toastrService: ToastrService, public translate: TranslateService,
+        @Host() private grid: GridComponent, private elRef: ElementRef) {
         super(toastrService);
     }
 
@@ -43,28 +44,75 @@ export class GridSettingComponent extends BaseComponent implements OnInit {
         else
             this.rtl = false;
 
+        var id: string = this.elRef.nativeElement.id + "_hidden";
+
+        var rowDataString = localStorage.getItem(id);
+        if (rowDataString)
+            this.rowData = JSON.parse(rowDataString);
 
         this.grid.leafColumns.toArray().forEach((item, index, arr) => {
             if (item.constructor.name == "ColumnComponent") {
-                var row = { "visibility": true, "name": item.displayTitle };
-                this.rowData.push(row);
+                var arrayIndex = this.rowData.findIndex(p => p.field == (<ColumnComponent>item).field)
+                var arrayItem: any = null;
+                if (arrayIndex >= 0)
+                    arrayItem = this.rowData[arrayIndex];
+
+                
+                var row = { visibility: true, name: item.displayTitle, field: (<ColumnComponent>item).field, disabled : false};
+                if (arrayItem) {
+                    row.visibility = arrayItem.visibility;
+                    this.rowData[arrayItem] = row;                    
+                    item.hidden = !row.visibility;
+
+                    var hiddenColumns = this.rowData.filter(p => p.visibility == false);
+                    
+                    if (row.visibility && hiddenColumns.length == this.rowData.length - 1)
+                        row.disabled = true;
+
+                }
+                else
+                    this.rowData.push(row);
             }
         });
+
+        
+        
         
     }
 
     changeVisibility(name :string, event : any) {
 
+
+        var id: string = this.elRef.nativeElement.id + "_hidden";
         var hidden: boolean;
         
         hidden = !event.target.checked;
         
         this.grid.columns.toArray().forEach((item, index, arr) => {
             if (item.constructor.name == "ColumnComponent") {
-                if ((<ColumnComponent>item).field == name)
+                if ((<ColumnComponent>item).field == name) {
                     item.hidden = hidden;
+
+                    var arrayIndex = this.rowData.findIndex(p => p.field == (<ColumnComponent>item).field)
+                    var arrayItem: any = null;
+                    this.rowData[arrayIndex].visibility = !hidden;
+
+                    var hiddenColumns = this.rowData.filter(p => p.visibility == false);
+                    if (hiddenColumns.length == this.rowData.length - 1) {
+                        var lastVisibleColumn = this.rowData.findIndex(p => p.visibility == true);
+                        this.rowData[lastVisibleColumn].disabled = true;
+                    }
+                    else {
+                        var lastDisableColumn = this.rowData.findIndex(p => p.disabled == true);
+                        if(lastDisableColumn >= 0)
+                            this.rowData[lastDisableColumn].disabled = false;
+
+                    }
+                }
             }
         });
+
+        localStorage.setItem(id, JSON.stringify(this.rowData));
 
     }
 
