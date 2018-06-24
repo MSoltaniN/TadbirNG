dc7df20744f9e274f151e053d581178816c2658d
saeed ghasemi <s.ghasemi365@gmail.com>
2018-06-24 10:48:47 +0430

    Fixed a bug in dateRangeSelector (UI - Build 252)

--- src/Framework/SPPC.Tadbir.Web/ClientApp/app/controls/dateRangeSelector/sppc-dateRangeSelector.ts
+++ src/Framework/SPPC.Tadbir.Web/ClientApp/app/controls/dateRangeSelector/sppc-dateRangeSelector.ts
@@ -35,6 +35,27 @@ export class SppcDateRangeSelector extends BaseFilterCellComponent implements On
     }
 
     ngOnInit(): void {
+
+        var lang: string = "fa";
+        if (localStorage.getItem('lang') != null) {
+            var item: string | null;
+            item = localStorage.getItem('lang');
+            if (item)
+                lang = item;
+        }
+        else
+            if (sessionStorage.getItem('lang') != null) {
+                var item: string | null;
+                item = sessionStorage.getItem('lang');
+                if (item)
+                    lang = item;
+            }
+
+        if (lang == "fa")
+            this.rtl = true;
+        else
+            this.rtl = false;
+        
         this.myForm = this.formBuilder.group({
             fromDate: '',
             toDate: ''
