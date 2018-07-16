8b81ff038b47e22a12bb53711f0477ea8f1e3535
b-eslamieh <b_eslamieh@sppc.co.ir>
2018-07-16 12:40:40 +0430

    Fixed missing XML doc warnings

--- src/Framework/SPPC.Framework.Common/Presentation/FilterExpression.cs
+++ src/Framework/SPPC.Framework.Common/Presentation/FilterExpression.cs
@@ -4,8 +4,14 @@ using System.Text;
 
 namespace SPPC.Framework.Presentation
 {
+    /// <summary>
+    /// اطلاعات یک عبارت فیلتر ساده یا مرکب مورد استفاده در فیلتر لیست های اطلاعاتی را نگهداری می کند
+    /// </summary>
     public class FilterExpression
     {
+        /// <summary>
+        /// نمونه جدیدی از این کلاس می سازد
+        /// </summary>
         public FilterExpression()
         {
             Filter = new GridFilter();
@@ -13,14 +19,30 @@ namespace SPPC.Framework.Presentation
             Children = new List<FilterExpression>();
         }
 
+        /// <summary>
+        /// عبارت فیلتر اصلی روی یکی از فیلد های اطلاعاتی
+        /// </summary>
         public GridFilter Filter { get; set; }
 
+        /// <summary>
+        /// عملگر مورد استفاده برای ترکیب این فیلتر با فیلترهای عبارت والد
+        /// </summary>
         public string Operator { get; set; }
 
+        /// <summary>
+        /// عبارت فیلتر والد در ساختار درختی یک فیلتر ترکیبی پیچیده
+        /// </summary>
         public FilterExpression Parent { get; set; }
 
+        /// <summary>
+        /// عبارات فیلتر اضافه که توسط عملگرهای منطقی "و" و "یا" قابل ترکیب با عبارت اصلی هستند
+        /// </summary>
         public IList<FilterExpression> Children { get; private set; }
 
+        /// <summary>
+        /// یک رشته متنی شامل مقادیر این نمونه ساخته و برمی گرداند
+        /// </summary>
+        /// <returns>یک رشته متنی شامل مقادیر این نمونه</returns>
         public override string ToString()
         {
             var builder = new StringBuilder(Filter.ToString());
--- src/Framework/SPPC.Framework.Common/Presentation/FilterExpressionBuilder.cs
+++ src/Framework/SPPC.Framework.Common/Presentation/FilterExpressionBuilder.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 
 namespace SPPC.Framework.Presentation
 {
+    /// <summary>
+    /// امکان ساخت یک عبارت فیلتر ساده یا ترکیبی را فراهم می کند
+    /// </summary>
     public class FilterExpressionBuilder
     {
+        /// <summary>
+        /// نمونه جدیدی از این کلاس می سازد
+        /// </summary>
         public FilterExpressionBuilder()
         {
         }
 
+        /// <summary>
+        /// عبارت فیلتر ساده یا ترکیبی ساخته شده توسط این نمونه
+        /// </summary>
         public FilterExpression Expression { get; private set; }
 
+        /// <summary>
+        /// متد موزد استفاده برای شروع ساخت عبارت فیلتر
+        /// </summary>
+        /// <param name="filter">عبارت شرطی مورد استفاده</param>
+        /// <returns>
+        /// نمونه فعلی که با استفاده از آن می توان متدهای بعدی را به صورت زنجیره ای فراخوانی کرد
+        /// </returns>
         public FilterExpressionBuilder New(GridFilter filter)
         {
             Expression = new FilterExpression()
@@ -21,6 +37,13 @@ namespace SPPC.Framework.Presentation
             return this;
         }
 
+        /// <summary>
+        /// امکان ترکیب عبارت فیلتر موجود با یک عبارت شرطی جدید را به کمک عملگر منطقی "و" فراهم می کند
+        /// </summary>
+        /// <param name="filter">عبارت شرطی جدید</param>
+        /// <returns>
+        /// نمونه فعلی که با استفاده از آن می توان متدهای بعدی را به صورت زنجیره ای فراخوانی کرد
+        /// </returns>
         public FilterExpressionBuilder And(GridFilter filter)
         {
             Expression.Children.Add(new FilterExpression()
@@ -32,12 +55,26 @@ namespace SPPC.Framework.Presentation
             return this;
         }
 
+        /// <summary>
+        /// امکان ترکیب عبارت فیلتر موجود با تعداد متغیری از عبارات شرطی جدید را به کمک عملگر منطقی "و" فراهم می کند
+        /// </summary>
+        /// <param name="filters">تعداد متغیری از عبارات شرطی جدید</param>
+        /// <returns>
+        /// نمونه فعلی که با استفاده از آن می توان متدهای بعدی را به صورت زنجیره ای فراخوانی کرد
+        /// </returns>
         public FilterExpressionBuilder And(params GridFilter[] filters)
         {
             Array.ForEach(filters, filter => And(filter));
             return this;
         }
 
+        /// <summary>
+        /// امکان ترکیب عبارت فیلتر موجود با یک عبارت شرطی جدید را به کمک عملگر منطقی "یا" فراهم می کند
+        /// </summary>
+        /// <param name="filter">عبارت شرطی جدید</param>
+        /// <returns>
+        /// نمونه فعلی که با استفاده از آن می توان متدهای بعدی را به صورت زنجیره ای فراخوانی کرد
+        /// </returns>
         public FilterExpressionBuilder Or(GridFilter filter)
         {
             Expression.Children.Add(new FilterExpression()
@@ -49,12 +86,23 @@ namespace SPPC.Framework.Presentation
             return this;
         }
 
+        /// <summary>
+        /// امکان ترکیب عبارت فیلتر موجود با تعداد متغیری از عبارات شرطی جدید را به کمک عملگر منطقی "یا" فراهم می کند
+        /// </summary>
+        /// <param name="filters">تعداد متغیری از عبارات شرطی جدید</param>
+        /// <returns>
+        /// نمونه فعلی که با استفاده از آن می توان متدهای بعدی را به صورت زنجیره ای فراخوانی کرد
+        /// </returns>
         public FilterExpressionBuilder Or(params GridFilter[] filters)
         {
             Array.ForEach(filters, filter => Or(filter));
             return this;
         }
 
+        /// <summary>
+        /// عبارت فیلتر ساخته شده را برمی گرداند
+        /// </summary>
+        /// <returns>عبارت فیلتر ساخته شده</returns>
         public FilterExpression Build()
         {
             return Expression;
