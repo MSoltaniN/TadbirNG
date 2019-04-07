aa142a67715b328a282e89c755afdf529dd42c2d
Saeed Ghasemi <s_ghasemi@sppc.co.ir>
2019-04-07 15:45:38 +0430

    Refactoring lookup api address in fullAccount.service (UI - Build 494)

--- src/Framework/SPPC.Tadbir.Web.New/ClientApp/src/app/service/fullAccount.service.js
+++ src/Framework/SPPC.Tadbir.Web.New/ClientApp/src/app/service/fullAccount.service.js
@@ -21,9 +21,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 require("rxjs/Rx");
-var source_1 = require("../class/source");
 var base_service_1 = require("../class/base.service");
-var index_1 = require("./api/index");
 var FullAccountInfo = /** @class */ (function () {
     function FullAccountInfo() {
     }
@@ -38,25 +36,29 @@ var FullAccountService = /** @class */ (function (_super) {
         return _this;
     }
     FullAccountService.prototype.GetAccountsLookup = function () {
-        var url = source_1.String.Format(index_1.LookupApi.FiscalPeriodBranchAccounts, this.FiscalPeriodId, this.BranchId);
+        //var url = String.Format(LookupApi.FiscalPeriodBranchAccounts, this.FiscalPeriodId, this.BranchId);
+        var url = "";
         var options = { headers: this.httpHeaders };
         return this.http.get(url, options)
             .map(function (response) { return response; });
     };
     FullAccountService.prototype.GetDetailAccountsLookup = function () {
-        var url = source_1.String.Format(index_1.LookupApi.FiscalPeriodBranchDetailAccounts, this.FiscalPeriodId, this.BranchId);
+        //var url = String.Format(LookupApi.FiscalPeriodBranchDetailAccounts, this.FiscalPeriodId, this.BranchId);
+        var url = "";
         var options = { headers: this.httpHeaders };
         return this.http.get(url, options)
             .map(function (response) { return response; });
     };
     FullAccountService.prototype.GetCostCentersLookup = function () {
-        var url = source_1.String.Format(index_1.LookupApi.FiscalPeriodBranchCostCenters, this.FiscalPeriodId, this.BranchId);
+        //var url = String.Format(LookupApi.FiscalPeriodBranchCostCenters, this.FiscalPeriodId, this.BranchId);
+        var url = "";
         var options = { headers: this.httpHeaders };
         return this.http.get(url, options)
             .map(function (response) { return response; });
     };
     FullAccountService.prototype.GetProjectsLookup = function () {
-        var url = source_1.String.Format(index_1.LookupApi.FiscalPeriodBranchProjects, this.FiscalPeriodId, this.BranchId);
+        //var url = String.Format(LookupApi.FiscalPeriodBranchProjects, this.FiscalPeriodId, this.BranchId);
+        var url = "";
         var options = { headers: this.httpHeaders };
         return this.http.get(url, options)
             .map(function (response) { return response; });
