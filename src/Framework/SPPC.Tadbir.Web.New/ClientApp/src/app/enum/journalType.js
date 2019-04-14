1e76c03a2d93fe0d9ad77a92373bfe0139545575
Saeed Ghasemi <s_ghasemi@sppc.co.ir>
2019-04-14 10:20:55 +0430

    Added all voucher to journal voucher status list (UI - Build 505)

--- src/Framework/SPPC.Tadbir.Web.New/ClientApp/src/app/enum/journalType.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var JournalType;
-(function (JournalType) {
-    JournalType["ByDate"] = "1";
-    JournalType["ByVoucher"] = "2";
-})(JournalType = exports.JournalType || (exports.JournalType = {}));
-//# sourceMappingURL=journalType.js.map
