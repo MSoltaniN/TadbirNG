7cdf98587e8c5fc12f42b2f667997c6cf7a4c8f8
Administrator <Administrator@ASUS-PC>
2019-04-22 16:31:22 +0430

    Resolve warning in ReportController (Build 552)

---
+++ src/Framework/SPPC.Tadbir.ViewModel/Reporting/QuickReportColumnViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPPC.Tadbir.ViewModel.Reporting
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class QuickReportColumnViewModel
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public string DefaultText { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public int Index { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public int SortMode { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public int SortOrder { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public bool Enabled { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public int Order { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public string UserText { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public int DataType { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public bool Visible { get; set; }
+    }
+}
