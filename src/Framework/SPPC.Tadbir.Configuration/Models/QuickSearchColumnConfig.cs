fba80bb9c0bfaa81bf7dfae0ecbd6a3eb461055b
b-eslamieh <b_eslamieh@sppc.co.ir>
2019-06-15 17:54:03 +0430

    Added initial types for search config (Build 626)

---
+++ src/Framework/SPPC.Tadbir.Configuration/Models/QuickSearchColumnConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SPPC.Tadbir.Configuration.Models
+{
+    /// <summary>
+    /// تنظیمات یک ستون قابل نمایش و قابل جستجو در فرم عمومی جستجو را نگهداری می کند
+    /// </summary>
+    public class QuickSearchColumnConfig
+    {
+        /// <summary>
+        /// نمونه جدیدی از این کلاس می سازد
+        /// </summary>
+        public QuickSearchColumnConfig()
+        {
+        }
+
+        /// <summary>
+        /// عنوان محلی شده ستون
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// ایندکس ترتیبی نمایش ستون در لیست نتایج جستجو، که از صفر شروع می شود
+        /// </summary>
+        public int DisplayIndex { get; set; }
+
+        /// <summary>
+        /// مشخص می کند که آیا ستون مورد نظر در لیست نتایج نمایش داده می شود یا نه؟
+        /// </summary>
+        public bool IsDisplayed { get; set; }
+
+        /// <summary>
+        /// مشخص می کند که آیا ستون مورد نظر برای جستجوی متنی مورد استفاده قرار می گیرد یا نه؟
+        /// </summary>
+        public bool IsSearched { get; set; }
+    }
+}
---
+++ src/Framework/SPPC.Tadbir.Configuration/Models/QuickSearchConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SPPC.Tadbir.Configuration.Models
+{
+    public class QuickSearchConfig
+    {
+        public QuickSearchConfig()
+        {
+            Columns = new List<QuickSearchColumnConfig>();
+        }
+
+        public int ViewId { get; set; }
+
+        public string SearchMode { get; set; }
+
+        public List<QuickSearchColumnConfig> Columns { get; }
+    }
+}
