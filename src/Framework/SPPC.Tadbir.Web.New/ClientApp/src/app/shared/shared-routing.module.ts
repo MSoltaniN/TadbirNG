54743717e7f9ea625bfdddc9b4c2c0ad3870a673
Saeed Ghasemi <s_ghasemi@sppc.co.ir>
2019-08-06 15:34:17 +0430

    Fixed a bug in shared-routing module (UI - Build 670)

--- src/Framework/SPPC.Tadbir.Web.New/ClientApp/src/app/shared/shared-routing.module.ts
+++ src/Framework/SPPC.Tadbir.Web.New/ClientApp/src/app/shared/shared-routing.module.ts
@@ -9,21 +9,21 @@ import { LoginContainerComponent } from './components/login/login.container.comp
 import { LogoutComponent } from './components/login/logout.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: '',
     component: LayoutComponent,
     canActivate: [AuthGuard],
     children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'dashboard', component: DashboardComponent },
       { path: 'home', component: HomeComponent },
-      { path: 'dashboard', component: DashboardComponent },
       { path: 'reports', component: ReportManagementComponent },
+      { path: 'login', component: LoginContainerComponent },
+      { path: 'logout', component: LogoutComponent },
     ]
   },
-  { path: 'login', component: LoginContainerComponent },
-  { path: 'logout', component: LogoutComponent },
-  { path: '**', redirectTo: 'dashboard' }
+
+
 ];
 
 @NgModule({
