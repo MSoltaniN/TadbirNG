c15a4d4e2856ce91d9bae456c146020c7cdcf0db
s-nouri <s.nouri2005@gmail.com>
2020-04-25 16:56:12 +0430

    fix some bugs in balancebyaccount (UI-Build 884)

--- src/Framework/TadbirSolutionInfo.cs
+++ src/Framework/TadbirSolutionInfo.cs
@@ -22,6 +22,6 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.883.0")]
-[assembly: AssemblyFileVersion("1.0.883.0")]
+[assembly: AssemblyVersion("1.0.884.0")]
+[assembly: AssemblyFileVersion("1.0.884.0")]
 
