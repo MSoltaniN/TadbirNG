154ea0852ae7a1f1295363a30d86488bff1cddef
b-eslamieh <babak.eslamieh@gmail.com>
2020-08-05 09:37:29 +0430

    FiscalPeriod API review (XML doc, cleanup, group delete) (Build 969)

--- src/Framework/SPPC.Tadbir.Interfaces/Persistence/IFiscalPeriodRepository.cs
+++ src/Framework/SPPC.Tadbir.Interfaces/Persistence/IFiscalPeriodRepository.cs
@@ -22,36 +22,11 @@ namespace SPPC.Tadbir.Persistence
         Task<PagedList<FiscalPeriodViewModel>> GetFiscalPeriodsAsync(GridOptions gridOptions = null);
 
         /// <summary>
-        /// به روش آسنکرون، تعداد دوره های مالی تعریف شده در شرکت مشخص شده را
-        /// از محل ذخیره خوانده و برمی گرداند
-        /// </summary>
-        /// <param name="companyId"> شناسه عددی یکی از شرکت های موجود</param>
-        /// <param name="gridOptions">گزینه های مورد نظر برای نمایش رکوردها در نمای لیستی</param>
-        /// <returns>تعداد دوره های مالی تعریف شده در شرکت مشخص شده</returns>
-        Task<int> GetCountAsync(int companyId, GridOptions gridOptions = null);
-
-        /// <summary>
-        /// به روش آسنکرون، تعداد اسناد مالی ایجاد شده در دوره مالی مشخص شده را خوانده و برمی گرداند
-        /// </summary>
-        /// <param name="fpId">شناسه دیتابیسی دوره مالی مورد نظر</param>
-        /// <returns>تعداد اسناد مالی ایجاد شده در دوره مالی</returns>
-        Task<int> GetVoucherCountAsync(int fpId);
-
-        /// <summary>
         /// به روش آسنکرون،دوره مالی با شناسه عددی مشخص شده را از محل ذخیره خوانده و برمی گرداند
         /// </summary>
         /// <param name="fperiodId">شناسه عددی یکی از دوره های مالی</param>
         /// <returns>دوره مالی مشخص شده با شناسه عددی</returns>
         Task<FiscalPeriodViewModel> GetFiscalPeriodAsync(int fperiodId);
-
-        /// <summary>
-        /// به روش آسنکرون، مشخص می کند که آیا در محدوده تاریخی داده شده دوره مالی تعریف شده یا نه
-        /// </summary>
-        /// <param name="start">شروع دوره مالی مورد نظر</param>
-        /// <param name="end">پایان دوره مالی مورد نظر</param>
-        /// <returns>اگر در محدوده تاریخی داده شده دوره مالی تعریف شده باشد، مقدار "درست"
-        /// و در غیر این صورت مقدار "نادرست" را برمی گرداند.</returns>
-        Task<bool> ExistsFiscalPeriodInRange(DateTime start, DateTime end);
 
         /// <summary>
         /// به روش آسنکرون، نقش های دارای دسترسی به یک دوره مالی را خوانده و برمی گرداند
