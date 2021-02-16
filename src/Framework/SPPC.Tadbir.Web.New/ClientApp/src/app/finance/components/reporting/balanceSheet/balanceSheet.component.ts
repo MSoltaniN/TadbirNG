d46cdf201c572e228471088a0ad228864fd0d658
s-nouri <s.nouri2005@gmail.com>
2021-02-16 20:57:07 +0330

    fixed some bugs in ProfitLoss and Balance sheet (UI-Build 1074)

--- src/Framework/SPPC.Tadbir.Web.New/ClientApp/src/app/finance/components/reporting/balanceSheet/balanceSheet.component.ts
+++ src/Framework/SPPC.Tadbir.Web.New/ClientApp/src/app/finance/components/reporting/balanceSheet/balanceSheet.component.ts
@@ -48,6 +48,7 @@ export function getLayoutModule(layout: Layout) {
 .section-account .acc-code-rtl { left: 30px; }
 .section-account .acc-code-ltr { right: 30px; }
 .section-account label {width:35%}
+/deep/ .k-grid td {direction:ltr!important}
 `],
   providers: [{
     provide: RTL,
