b4994f81583190b71a7f55fd7d6c0fe5ed8c1cd9
s-nouri <s.nouri2005@gmail.com>
2021-04-05 13:14:20 +0430

    fixed a display issue in boolean columns in quick report (UI-Build 1106)

--- src/Framework/SPPC.Tadbir.Web.New/ClientApp/src/app/shared/components/reportViewer/reportViewer.component.ts
+++ src/Framework/SPPC.Tadbir.Web.New/ClientApp/src/app/shared/components/reportViewer/reportViewer.component.ts
@@ -238,11 +238,13 @@ export class ReportViewerComponent extends DefaultComponent implements OnInit {
             || c.dataType.toLowerCase() === "system.date"));
 
           var timeSpanColumns = quickReportViewInfo.columns.filter(c => c.dataType && (c.dataType.toLowerCase() === "system.timespan"));
-          
+          var boolColumns = quickReportViewInfo.columns.filter(c => c.dataType && (c.dataType.toLowerCase() === "system.boolean"));
+
           if (dateColumns.length > 0 && this.CurrentLanguage == "fa") {
             var convertedData = reportRows;
             convertedData = this.convertToShamsiDate(convertedData, dateColumns);
             convertedData = this.formatTimeSpan(convertedData, timeSpanColumns);
+            convertedData = this.formatBoolean(convertedData, boolColumns);
 
             this.report.regData("data", "data", convertedData);
             registerData = true;
@@ -251,6 +253,8 @@ export class ReportViewerComponent extends DefaultComponent implements OnInit {
             var convertedData = reportRows;
             convertedData = this.convertToMiladiDate(convertedData, dateColumns);
             convertedData = this.formatTimeSpan(convertedData, timeSpanColumns);
+            convertedData = this.formatBoolean(convertedData, boolColumns);
+
             this.report.regData("data", "data", convertedData);
             registerData = true;
           }
@@ -319,6 +323,8 @@ export class ReportViewerComponent extends DefaultComponent implements OnInit {
           rows[index][item.name] = momentDate;
         }
       })
+
+
     }
     return rows;
   }
@@ -330,6 +336,24 @@ export class ReportViewerComponent extends DefaultComponent implements OnInit {
         if (rows[index][item.name]) {
           var time = (rows[index][item.name]).toString().split('.')[0];          
           rows[index][item.name] = time;
+        }
+      })
+    }
+    return rows;
+  }
+
+  formatBoolean(rows: any, cols: Array<QuickReportColumnConfig>) {
+    let trueText = this.getText('Report.TrueLabel');
+    let falseText = this.getText('Report.FalseLabel');
+
+    for (var index = 0; index < rows.length; index++) {
+      cols.forEach(function (item) {
+        if (rows[index][item.name] != null) {          
+          if (rows[index][item.name] == true)
+            rows[index][item.name] = trueText;
+
+          if (rows[index][item.name] == false)
+            rows[index][item.name] = falseText;
         }
       })
     }
