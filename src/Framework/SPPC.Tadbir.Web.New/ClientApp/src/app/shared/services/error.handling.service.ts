034455d166215233407dc58c8dd525f92e51d2e5
s-nouri <s.nouri2005@gmail.com>
2021-06-19 15:01:59 +0430

    added a global error message for all http request (UI-Build 1140)

--- src/Framework/SPPC.Tadbir.Web.New/ClientApp/src/app/shared/services/error.handling.service.ts
+++ src/Framework/SPPC.Tadbir.Web.New/ClientApp/src/app/shared/services/error.handling.service.ts
@@ -2,13 +2,20 @@ import { MessageService } from "@progress/kendo-angular-l10n";
 import { Error, ErrorType } from "@sppc/shared/models";
 import { ToastrService } from "ngx-toastr";
 import { Injectable } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Injectable()
-export class ErrorHandlingService  {
-
-  constructor(public toastrService: ToastrService) { }
+export class ErrorHandlingService {
+  globalErrorMessage: string;
+  constructor(public toastrService: ToastrService, public translateService: TranslateService) {
+    
+    this.translateService.get('Messages.GlobalErrorMessage').subscribe((msg: string) => {
+      this.globalErrorMessage = msg;
+    });
+  }
 
   public handleError(error: Error | any) {
+    debugger;
     if (error.statusCode == 400) {
       if (error.type) {
         switch (error.type) {
@@ -24,6 +31,11 @@ export class ErrorHandlingService  {
 
     if (error.statusCode == 500) {
       this.toastrService.error(error.Message);
+      return;
+    }
+
+    if (error && error.type == 'error') {
+      return this.globalErrorMessage;      
     }
 
     return undefined;
