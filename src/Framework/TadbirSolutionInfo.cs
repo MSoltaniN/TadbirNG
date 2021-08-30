e51d8f10ced21f43bbb8b1b20d627f44da81b335
s-nouri <s.nouri2005@gmail.com>
2021-08-30 17:23:52 +0430

    fixed a problem for showing VoucherStd report (UI-Build 1211)

--- src/Framework/TadbirSolutionInfo.cs
+++ src/Framework/TadbirSolutionInfo.cs
@@ -22,6 +22,6 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1210.0")]
-[assembly: AssemblyFileVersion("1.1.1210.0")]
+[assembly: AssemblyVersion("1.1.1211.0")]
+[assembly: AssemblyFileVersion("1.1.1211.0")]
 
