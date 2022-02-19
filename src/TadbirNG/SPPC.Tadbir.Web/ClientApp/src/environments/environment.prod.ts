43a9bbf36e2b98fa85f2756a9cf96c5d7c04688a
s-nouri <s.nouri2005@gmail.com>
2022-02-19 14:52:41 +0330

    Fixed some bugs in stimulsoft report and an issue in BalanceByAccount (UI-Build 1311)

--- src/TadbirNG/SPPC.Tadbir.Web/ClientApp/src/environments/environment.prod.ts
+++ src/TadbirNG/SPPC.Tadbir.Web/ClientApp/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@
 //     Runtime Version: 1.1.1222
 //     Template Version: 1.0
 //     Generation Date: 2021-09-13 4:06:58 PM
-//  
+//
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
 // </auto-generated>
@@ -12,9 +12,10 @@
 
 export const environment = {
   production: true,
-  BaseUrl: 'http://130.185.76.7:9095',  
-  LicenseServerUrl: 'http://130.185.76.7:9093',
-  InstanceKey: 'Llxw9MF0sjrg6mzvPl+bWDAKSWU2BR2NZpBYHFKUB/qaeAmcLqw7j4JzAn2LVTxNYmadslsODCW2PVlPfhTbdQZVIqB+JoXLy2NMRT7mJGVa2Fy3XJybj7ZVKNvJ+ByOj5g0NGn8+ygw4UfjKTjAV4LP58Y5qoScBTh0t2xiwCf+9kl0wPQ9oFm3/UpjkdwVMtU4mhNNtT8E1OZjD36cMQ==',
-  Version: '1.2.1310.0',
-  SessionAliveInterval:300000
+  BaseUrl: "http://130.185.76.7:9095",
+  LicenseServerUrl: "http://130.185.76.7:9093",
+  InstanceKey:
+    "Llxw9MF0sjrg6mzvPl+bWDAKSWU2BR2NZpBYHFKUB/qaeAmcLqw7j4JzAn2LVTxNYmadslsODCW2PVlPfhTbdQZVIqB+JoXLy2NMRT7mJGVa2Fy3XJybj7ZVKNvJ+ByOj5g0NGn8+ygw4UfjKTjAV4LP58Y5qoScBTh0t2xiwCf+9kl0wPQ9oFm3/UpjkdwVMtU4mhNNtT8E1OZjD36cMQ==",
+  Version: "1.2.1311.0",
+  SessionAliveInterval: 300000,
 };
