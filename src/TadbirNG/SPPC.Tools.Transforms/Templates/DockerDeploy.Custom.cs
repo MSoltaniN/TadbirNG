0e39898a7fd1a5ab77316828d81d63c52ba7a5a1
b-eslamieh <babak.eslamieh@gmail.com>
2022-03-12 10:49:45 +0330

    Generated simple deployment script in License Manager tool

---
+++ src/TadbirNG/SPPC.Tools.Transforms/Templates/DockerDeploy.Custom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SPPC.Tools.Transforms.Templates
+{
+    public partial class DockerDeploy : ITextTemplate
+    {
+        public DockerDeploy(string licenseKey)
+        {
+            _imageGuid = licenseKey
+                .ToLower()
+                .Substring(0, 8);
+        }
+
+        private readonly string _imageGuid;
+    }
+}
