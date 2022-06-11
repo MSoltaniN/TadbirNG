3e4aae28f3f0f44e71152153a8d65a64fe15a1fd
s-nouri <s.nouri2005@gmail.com>
2022-06-11 13:15:49 +0430

    Fixed some bugs in Voucher component

--- src/TadbirNG/SPPC.Tadbir.Web/ClientApp/src/app/shared/directive/button/buttonDisable.directive.ts
+++ src/TadbirNG/SPPC.Tadbir.Web/ClientApp/src/app/shared/directive/button/buttonDisable.directive.ts
@@ -1,23 +1,23 @@
-import { Directive, ElementRef, HostListener } from "@angular/core";;
-
-@Directive({ selector: 'button.k-button , button[kendogridaddcommand]' })
+import { Directive, ElementRef, HostListener } from "@angular/core";
+@Directive({ selector: "button.k-button , button[kendogridaddcommand]" })
 export class SppcButtonDisable {
-   
+  constructor(public element: ElementRef) {}
 
-  constructor(public element: ElementRef) {
-        
-  }   
+  @HostListener("click") onClick() {
+    if (this.element.nativeElement.classList.contains("not-change")) {
+      return;
+    }
 
-  @HostListener('click') onClick() {    
     this.element.nativeElement.disabled = true;
-    this.element.nativeElement.classList.add("current-clicked-button");    
+    this.element.nativeElement.classList.add("current-clicked-button");
 
     setTimeout(() => {
-      if (this.element.nativeElement.classList.contains("current-clicked-button")) {
+      if (
+        this.element.nativeElement.classList.contains("current-clicked-button")
+      ) {
         this.element.nativeElement.disabled = false;
-        this.element.nativeElement.classList.remove("current-clicked-button");        
+        this.element.nativeElement.classList.remove("current-clicked-button");
       }
     }, 350);
-
   }
 }
