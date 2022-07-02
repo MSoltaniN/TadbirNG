75c6a2156c04d488e0bcfcfe8c8a3e379b80ef15
Abbas Aghayari <Abbas2017@gmail.com>
2022-07-02 11:41:58 +0430

    fix a bug in edit companies item

--- src/TadbirNG/SPPC.Tadbir.Web/ClientApp/src/app/organization/components/company/company-form.component.ts
+++ src/TadbirNG/SPPC.Tadbir.Web/ClientApp/src/app/organization/components/company/company-form.component.ts
@@ -53,9 +53,6 @@ export class CompanyFormComponent extends DetailComponent implements OnInit {
 
     setTimeout(() => {
       this.editForm.reset(this.model);
-      if (!this.isNew) {
-        this.editForm.get('dbName').disable()
-      }
       this.editForm.get('dbName').setValidators([Validators.required, Validators.maxLength(128), Validators.pattern("^[a-zA-Z-_]+$")]);
     })
   }
