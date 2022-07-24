9c1de0e950d530d5bb437eeb45c99e559f0669b2
Abbas Aghayari <Abbas2017@gmail.com>
2022-07-24 12:42:28 +0430

    loazy load is disabled for some issues

--- src/TadbirNG/SPPC.Tadbir.Web/ClientApp/src/app/config/config-routing.module.ts
+++ src/TadbirNG/SPPC.Tadbir.Web/ClientApp/src/app/config/config-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SettingsComponent } from '@sppc/config/components/settings/settings.component';
+import { AuthGuard } from '@sppc/core';
+import { LayoutComponent } from '@sppc/shared/components/layout/layout.component';
 
 const routes: Routes = [
-    { path: 'settings', component: SettingsComponent },
+  { path: 'config', component: LayoutComponent, canActivate: [AuthGuard],
+    children: [
+      { path: 'settings', component: SettingsComponent },
+    ]
+  }
   ];
 
 @NgModule({
