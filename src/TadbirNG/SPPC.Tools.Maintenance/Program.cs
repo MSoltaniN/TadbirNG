0e21891c570f6d17381582d4e2a0b17568252f3f
b-eslamieh <babak.eslamieh@gmail.com>
2022-09-08 19:25:52 +0430

    Added maintenance tool for backup/restore of vital data in Win VPS (Build 1434)

---
+++ src/TadbirNG/SPPC.Tools.Maintenance/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using SPPC.Framework.Extensions;
+
+namespace SPPC.Tools.Maintenance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayBanner();
+            if (args.Length == 0 || args[0] == "backup")
+            {
+                RunBackup();
+            }
+        }
+
+        static void DisplayBanner()
+        {
+            Console.WriteLine();
+            Console.WriteLine("============================================================");
+            Console.WriteLine("SPPC Server Maintenance Utility (v1.0)");
+            Console.WriteLine($"(c) Copyright {DateTime.Now.Year}, SPPC, All Rights Reserved");
+            Console.WriteLine("============================================================");
+            Console.WriteLine();
+        }
+
+        static void RunBackup()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var backup = new BackupUtility();
+            backup.TaskStarted += Backup_TaskStarted;
+            backup.TaskFinished += Backup_TaskFinished;
+            backup.FtpProgress += Backup_FtpProgress;
+            backup.BackupNgTadbirDatabases();
+            backup.BackupNgTadbirSites();
+            var tempPath = Path.Combine(".", "temp");
+            if (Directory.Exists(tempPath))
+            {
+                Directory.Delete(tempPath);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine("Backup completed.");
+            Console.WriteLine($"Elapsed : {stopwatch.Elapsed}");
+        }
+
+        private static void Backup_TaskStarted(object sender, TaskStartedEventArgs e)
+        {
+            var current = SetConsoleColor(ConsoleColor.Blue);
+            Console.WriteLine($"{e.TaskType} {e.TargetType} '{e.Name}'");
+            ResetConsoleColor(current);
+        }
+
+        private static void Backup_TaskFinished(object sender, TaskFinishedEventArgs e)
+        {
+            if (e.Succeeded)
+            {
+                var current = SetConsoleColor(ConsoleColor.Green);
+                Console.WriteLine($"(Succeeded) {e.TaskType} {e.TargetType} '{e.Name}'");
+                ResetConsoleColor(current);
+                Console.WriteLine();
+            }
+            else
+            {
+                var current = SetConsoleColor(ConsoleColor.DarkRed);
+                Console.WriteLine($"(Failed) {e.TaskType} {e.TargetType} '{e.Name}'");
+                Console.WriteLine(e.Exception.GetErrorInfo());
+                Console.WriteLine();
+                ResetConsoleColor(current);
+            }
+        }
+
+        private static void Backup_FtpProgress(object sender, FtpProgressEventArgs e)
+        {
+            var current = SetConsoleColor(ConsoleColor.Blue);
+            Console.Write($"\r{e.Message}    ");
+            ResetConsoleColor(current);
+            if (e.IsComplete)
+            {
+                Console.WriteLine();
+            }
+        }
+
+        private static ConsoleColor SetConsoleColor(ConsoleColor color)
+        {
+            var currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            return currentColor;
+        }
+
+        private static void ResetConsoleColor(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+        }
+    }
+}
---
+++ src/TadbirNG/SPPC.Tools.Maintenance/Utility/SqlServerUtility.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.SqlServer.Management.Smo;
+using SPPC.Framework.Common;
+
+namespace SPPC.Tools.Maintenance
+{
+    public class SqlServerUtility
+    {
+        public SqlServerUtility(string instanceName)
+        {
+            Verify.ArgumentNotNullOrEmptyString(instanceName, nameof(instanceName));
+            _instanceName = instanceName;
+        }
+
+        public string[] GetDbNames()
+        {
+            var server = new Server(_instanceName);
+            return server.Databases
+                .Cast<Database>()
+                .Select(db => db.Name)
+                .ToArray();
+        }
+
+        public string BackupDatabase(string dbName, DbBackupOptions options)
+        {
+            Verify.ArgumentNotNull(dbName, nameof(dbName));
+            Verify.ArgumentNotNull(options, nameof(options));
+            var server = new Server(_instanceName);
+            var backup = new Backup()
+            {
+                Action = options.Action,
+                BackupSetDescription = options.Description,
+                BackupSetName = options.Name,
+                Database = dbName,
+                ExpirationDate = options.ExpirationDate,
+                Incremental = options.Incremental,
+                LogTruncation = options.LogTruncation
+            };
+            var backupPath = Path.Combine(server.BackupDirectory, options.FileName);
+            var device = new BackupDeviceItem(backupPath, DeviceType.File);
+            backup.Devices.Add(device);
+            backup.SqlBackup(server);
+            return backupPath;
+        }
+
+        private readonly string _instanceName;
+    }
+}
