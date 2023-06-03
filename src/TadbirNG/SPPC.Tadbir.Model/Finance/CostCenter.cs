b92b0c0a0d323eeef058e07ccb8ccd8374504ad2
b-eslamieh <babak.eslamieh@gmail.com>
2023-06-03 22:10:07 +0330

    [Refactoring] Removed some duplicate codes from 4 repositories (Build 1527)

--- src/TadbirNG/SPPC.Tadbir.Model/Finance/CostCenter.cs
+++ src/TadbirNG/SPPC.Tadbir.Model/Finance/CostCenter.cs
@@ -8,10 +8,5 @@ namespace SPPC.Tadbir.Model.Finance
         /// مجموعه ای از مراکز هزینه زیرشاخه (فرزند) در ساختار درختی
         /// </summary>
         public IList<CostCenter> Children { get; protected set; }
-
-        /// <summary>
-        /// شناسه دیتابیسی مرکز هزینه والد در ساختار درختی
-        /// </summary>
-        public int? ParentId { get; set; }
     }
 }
---
+++ src/TadbirNG/SPPC.Tadbir.Persistence/NumericText.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SPPC.Framework.Common;
+using SPPC.Tadbir.Domain;
+
+namespace SPPC.Tadbir.Persistence
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NumericText
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="length"></param>
+        public NumericText(int length)
+        {
+            Verify.ArgumentNotOutOfRange(length, 1, AppConstants.MaxCodeLength, nameof(length));
+            Length = length;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Length { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="existingValues"></param>
+        /// <returns></returns>
+        public string GetNewValue(IEnumerable<string> existingValues)
+        {
+            string format = String.Format("D{0}", Length);
+            var maxValue = (long)Math.Pow(10, Length) - 1;
+            var lastValue = existingValues.Any()
+                ? Int64.Parse(existingValues.Max())
+                : 0;
+            var newValue = Math.Min(lastValue + 1, maxValue);
+            return newValue.ToString(format);
+        }
+    }
+}
