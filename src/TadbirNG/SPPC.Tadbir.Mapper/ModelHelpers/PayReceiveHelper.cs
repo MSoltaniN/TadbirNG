053a2db4bcb28d7eeaadcf1ccf3d9f574cf0ce1d
Maryam Eftekhari <maryameftekhari.me90@gmail.com>
2023-07-24 16:57:06 +0330

    Added sum article amounts for PayReceive API Build(1552)

---
+++ src/TadbirNG/SPPC.Tadbir.Mapper/ModelHelpers/PayReceiveHelper.cs
@@ -0,0 +1,28 @@
+﻿using SPPC.Framework.Common;
+using SPPC.Tadbir.Model.CashFlow;
+using SPPC.Tadbir.Model.Finance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPPC.Tadbir.Mapper.ModelHelpers
+{
+    internal class PayReceiveHelper
+    {
+        internal static decimal GetAccountAmountsSum(PayReceive payReceive)
+        {
+            Verify.ArgumentNotNull(payReceive, nameof(payReceive));
+            return payReceive.Accounts
+                .Sum(acc => acc.Amount);
+        }
+
+        internal static decimal GetCashAmountsSum(PayReceive payReceive)
+        {
+            Verify.ArgumentNotNull(payReceive, nameof(payReceive));
+            return payReceive.CashAccounts
+                .Sum(cashAcc => cashAcc.Amount);
+        }
+    }
+}
