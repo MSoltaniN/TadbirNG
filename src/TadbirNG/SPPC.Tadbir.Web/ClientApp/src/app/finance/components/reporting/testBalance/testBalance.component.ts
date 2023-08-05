29536a1eee75bc48802e86d2568d6fba9e2e818d
MohammadReza <mohammadreza@mdra.ir>
2023-08-05 05:57:13 -0700

    debug labels , titles and messages

--- src/TadbirNG/SPPC.Tadbir.Web/ClientApp/src/app/finance/components/reporting/testBalance/testBalance.component.ts
+++ src/TadbirNG/SPPC.Tadbir.Web/ClientApp/src/app/finance/components/reporting/testBalance/testBalance.component.ts
@@ -1269,7 +1269,7 @@ export class TestBalanceComponent
               String.Format(
                 this.getText("Messages.VoucherNumberByStatus"),
                 res.toString(),
-                this.getText(statusFilter.key),
+                this.getText(statusFilter.key).replace("اسناد","").replace("voucheres",""),
                 statusFilter.url
               ),
               MessageType.Info
