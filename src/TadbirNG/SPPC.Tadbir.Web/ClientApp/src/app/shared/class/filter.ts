6a0a2b81b66e1bea2acd4348b2b60b8ffd769774
MohammadReza <mohammadreza@mdra.ir>
2023-08-19 11:57:55 +0330

    Fixed issue 854

--- src/TadbirNG/SPPC.Tadbir.Web/ClientApp/src/app/shared/class/filter.ts
+++ src/TadbirNG/SPPC.Tadbir.Web/ClientApp/src/app/shared/class/filter.ts
@@ -1,5 +1,6 @@
 import { Braces } from "../models";
 import './string.extensions';
+import * as moment from "jalali-moment";
 //*** this class add for filter values for gridview */
 
 export class Filter{    
@@ -8,5 +9,11 @@ export class Filter{
     public braces: Array<Braces> = null, public id: string = null)
     {    
       this.Value = this.Value.toString().replaceBadChars(this.Value);    
+       if(localStorage.getItem('lang')=="fa" && fieldTypeName.toLocaleLowerCase()=="system.date"){
+         debugger;
+         let jalaliDate = this.Value ;
+         let gregorianDate = moment.from(jalaliDate, 'fa', 'YYYY/MM/DD').locale('en').format('YYYY/MM/DD');
+         this.Value=gregorianDate;
+       }
     }
 }
