5120b8a0f997178c0396df6054e5f428f07aea50
m-eftekhari <maryameftekhari.me90@gmail.com>
2023-09-17 17:20:15 +0330

    Add models for PayReceive config

---
+++ src/TadbirNG/SPPC.Tadbir.Configuration/Models/PaymentConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPPC.Tadbir.Configuration.Models
+{
+    /// <summary>
+    /// تنظیمات فرآیند و ثبت مالی فرم پرداخت را نگهداری می‌کند
+    /// </summary>
+    public class PaymentConfig
+    {
+        /// <summary>
+        /// تنظیمات فرآیند ثبت مالی فرم پرداخت
+        /// </summary>
+        public RegisterFlowConfig RegisterFlowConfig { get; set; }
+
+        /// <summary>
+        /// سیاست‌های انتخاب یا ایجاد سند برای ثبت مالی فرم پرداخت
+        /// </summary>
+        public RegisterConfig RegisterConfig { get; set; }
+    }
+}
---
+++ src/TadbirNG/SPPC.Tadbir.Configuration/Models/RegisterConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPPC.Tadbir.Configuration.Models
+{
+    /// <summary>
+    /// سیاست‌های انتخاب یا ایجاد سند برای ثبت مالی در فرم‌های عملیاتی را نگهداری می‌کند
+    /// </summary>
+    public class RegisterConfig
+    {
+        /// <summary>
+        /// ارسال ثبت‌های مالی به آخرین سند باز معتبر
+        /// </summary>
+        public bool RegisterOnLastValidVoucher { get; set; }
+
+        /// <summary>
+        /// ایجاد یک سند حسابداری جدید برای هر فرم عملیاتی
+        /// </summary>
+        public bool RegisterOnCreatedVoucher { get; set; }
+        
+        /// <summary>
+        /// ثبت خودکار سند حسابداری ایجاد شده
+        /// </summary>
+        public bool CheckedVoucher { get; set; }
+    }
+}
